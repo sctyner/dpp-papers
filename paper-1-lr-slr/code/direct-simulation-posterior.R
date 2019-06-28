5dd353a34460d2040ef730565e806740e4aa55ec
Sam Tyner <sctyner@iastate.edu>
2019-06-28 13:33:22 -0400

    commit work from before lunch

--- paper-1-lr-slr/code/direct-simulation-posterior.R
+++ paper-1-lr-slr/code/direct-simulation-posterior.R
@@ -1,101 +1,54 @@
 # simulating from the posterior that is "straight to the posterior" per Terenin & Draper 
 # DP(n ,\hat{F}_n)
 library(tidyverse)
-glass_pop <- read_csv("paper-1-lr-slr/dat/glass_pop_evett.csv")
-head(glass_pop)
-
-glass_pop %>% slice(rep(1:n(), each = Count))
-
-glass_pop_all <- rep(glass_pop$RI, glass_pop$Count)
-
-n <- length(glass_pop_all)
-
-ecdf_glass <- ecdf(glass_pop_all)
-
-
-draw_from_ecdf <- function(ecdf, n){
-  quantile(x = ecdf, probs = runif(n), type = 1)
-}
-
-
 library(dprocsim)
-pop_cdf_sims <- dpprior_sim2(M = n , F0 = draw_from_ecdf , sticks = 100, N = 500, ecdf = ecdf_glass)
+# population medians: g(theta) where theta = median(RI) in a pane
+glass_pop <- read_csv("paper-1-lr-slr/dat/glass_pop_evett.csv")
+#head(glass_pop)
+glass_pop_all <- rep(glass_pop$RI, glass_pop$Count)
+# number of obs in population
+n_pop <- length(glass_pop_all)
+# empirical cdf function 
+ecdf_glass_pop <- ecdf(glass_pop_all)
 
+# Step 1: simulate from posterior for g(theta)
 # draws from the posterior for g(theta)
+pop_cdf_sims <- dpprior_sim2(M = n_pop , F0 = draw_from_ecdf , sticks = 100, N = 500, ecdf = ecdf_glass_pop)
+
+# plot draws from the posterior for g(theta)
 pop_cdf_sims %>% unnest() %>% ggplot(aes(x = locations, y = weCDF, group=rep))+ 
   geom_line(alpha = .05)
 
-glass_window <- read_csv("paper-1-lr-slr/dat/window.csv")
+# repeated measurements from a single pane: 
+data("bennett.df", package = "dafs")
+glass_pane <- as.numeric(bennett.df[1,])
+median_pane <- median(glass_pane)
+glass_pane <- glass_pane - median_pane
+n_pane <- length(glass_pane)
+ecdf_glass_pane <- ecdf(glass_pane)
 
-n <- length(glass_window$RI)
+# Step 2: simulate from posterior for f(x-theta_0|theta=0)
+# draws from the posterior for f(x|theta)
+pane_cdf_sims <- dpprior_sim2(M = n_pane , F0 = draw_from_ecdf , sticks = 100, N = 500, ecdf = ecdf_glass_pane)
 
-ecdf_window <- ecdf(glass_window$RI)
-
-window_cdf_sims <- dpprior_sim2(M = n, F0 = draw_from_ecdf, sticks = 100, N = 500, ecdf = ecdf_window)
-
-
-# draws from the posterior for f(y | theta) (in the denominator, left hand side)
-window_cdf_sims %>% unnest() %>% ggplot(aes(x = locations, y = weCDF, group=rep))+ 
+# plot draws from the posterior for g(theta)
+pane_cdf_sims %>% unnest() %>% ggplot(aes(x = locations, y = weCDF, group=rep))+ 
   geom_line(alpha = .05)
 
-suspect_samp <- c(1.51848, 1.51850, 1.51848, 1.51844, 1.51846)
-
-n <- length(suspect_samp)
-
-ecdf_suspect <- ecdf(suspect_samp)
-
-suspect_sims <- dpprior_sim2(M = n, F0 = draw_from_ecdf, 
-                             sticks = 100, N = 500, ecdf = ecdf_suspect)
-
-
-# draws from the posterior for f(x | theta) (in the denominator, right hand side)
-suspect_sims %>% unnest() %>% ggplot(aes(x = locations, y = weCDF, group=rep))+ 
-  geom_line(alpha = .05)
-
-# combined 
-
-combined_glass <- c(suspect_samp, glass_window$RI)
-
-n <- length(combined_glass)
-
-ecdf_combined <- ecdf(combined_glass)
-
-combined_sims <- dpprior_sim2(M = n, F0 = draw_from_ecdf, 
-                             sticks = 100, N = 500, ecdf = ecdf_combined)
-
-combined_sims %>% unnest() %>% ggplot(aes(x = locations, y = weCDF, group=rep))+ 
-  geom_line(alpha = .05)
-
-ggplot(mapping = aes(x = locations, y = weCDF, group=rep))+ 
-  geom_line(data = unnest(combined_sims), alpha = .05) + 
-  geom_line(data = unnest(suspect_sims), alpha = .05, color = "red") + 
-  geom_line(data = unnest(window_cdf_sims), alpha = .05, color = "blue")+ 
-  geom_line(data = unnest(pop_cdf_sims), alpha = .05, color = "green")
-
+# places at which to evaluate f(x|theta) (when evaluating, subtract theta from each of these.)
+crime_scene_sample <- read_csv("paper-1-lr-slr/dat/window.csv")
+suspect_sample <- c(1.51848, 1.51850, 1.51848, 1.51844, 1.51846)
 
 #### simulating & computing the numerator (integrate f(x,y| theta)*g(theta) wrt theta)
 
-n_tot <- length(combined_glass)
-Fnum <- dpprior_sim(M = n_tot, F0 = draw_from_ecdf,sticks = 100, ecdf = ecdf_combined)
-
-n_pop <- length(glass_pop_all)
-gnum <- dpprior_sim(M = n_pop, F0 = draw_from_ecdf, sticks = 100, ecdf = ecdf_glass)
-gnum <- mutate(gnum, rep = 1)
-
-range(glass_pop_all)
-range(combined_glass)
-
 grid_theta <- seq(min(glass_pop_all), max(glass_pop_all), by = .00001)
-
-g_eval_grid <- tibble(theta = grid_theta) %>% 
-  mutate(dens_eval_gnum = map_dbl(theta, eval_dens, epsilon = .0001, sims = gnum))
 
 g_eval_grid <- tibble(theta = grid_theta) %>% 
   mutate(dens_eval_gnum = map(theta, eval_dens, epsilon = .0001, sims = pop_cdf_sims))
 
 head(g_eval_grid) 
 
-xbar = mean(combined_glass)
+
 
 g_eval_grid <- g_eval_grid %>% 
   mutate(dens_eval_num =  map_dbl(theta, dnorm, x = xbar, sd = 4e-5, log = FALSE))
@@ -123,33 +76,4 @@ g_eval_grid %>% filter(dens_eval_num != max(dens_eval_num)) %>% pull(dens_eval_n
 
 dnorm(x= xbar, mean = g_eval_grid$theta[1037], sd = 4e-5)
 
-sims <- gnum
-x = 1.51748
-epsilon <- .0001
-eval_dens <- function(x, epsilon, sims, ...) {
-  passing_sims <- sims %>%
-    unnest() %>%
-    mutate(in_interval = map_lgl(locations, # column to pass
-                                 in_range, # to this function
-                                 fixed_value = x, epsilon = epsilon
-    )) %>% # other args passed to the function
-    filter(in_interval)
   
-  if (nrow(passing_sims) == 0){
-    return(0)
-  } 
-  
-  dens_ests <- passing_sims %>%
-    group_by(rep) %>%
-    summarize(
-      min_loc = locations[which.min(locations)],
-      min_cdf = weCDF[which.min(locations)],
-      max_loc = locations[which.max(locations)],
-      max_cdf = weCDF[which.max(locations)]
-    ) %>%
-    mutate(dens_est = max_cdf - min_cdf) %>%
-    pull(dens_est)
-  
-  
-}
-
